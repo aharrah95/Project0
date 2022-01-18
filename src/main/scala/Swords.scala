882ed262ea5f47304a774c6470e1a90d8b94bd3b
aharrah95 <abigail.harrah@revature.net>
2022-01-18 16:18:08 -0600

    Swords general functions

--- src/main/scala/Swords.scala
+++ src/main/scala/Swords.scala
@@ -1,7 +1,12 @@
 class Swords {
     var swordRange: String = "Close-range weapon"
-    var basicLevel: String = "Level: Novice"
-    var midLevel: String = "Level: Expert"
-    var eliteLevel: String = "Level: Elite"
+    var basicLevelSword: String = "Level: Novice"
+    var midLevelSword: String = "Level: Expert"
+    var eliteLevelSword: String = "Level: Elite"
+
+    var basicLevelSwordPrice: Int = 250
+    var midLevelSwordPrice: Int = 10000
+    var eliteLevelSwordPrice: Int = 30000
+
 
 }
