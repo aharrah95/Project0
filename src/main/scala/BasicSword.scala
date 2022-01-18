a5097927c279c1e00ec8530a434332a58b8b0fa1
aharrah95 <abigail.harrah@revature.net>
2022-01-18 16:55:33 -0600

    Basic Sword code

---
+++ src/main/scala/BasicSword.scala
@@ -0,0 +1,10 @@
+object BasicSword extends Swords {
+
+  val name = "Blacksmith Standard"
+  val basicSwordSum = "The ideal weapon for novice adventurers. Its blade responds swiftly to strike down monsters, but it will break if not handled carefully."
+  val basicSwordMaterial = "Iron"
+
+  println(name + basicSwordSum)
+  println(basicSwordMaterial + swordRange + swordLevel + basicSwordPrice)
+
+}
