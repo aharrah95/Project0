b90bc371ffbc7de4c045274183ccbc84c5f9e8b4
aharrah95 <abigail.harrah@revature.net>
2022-01-18 16:55:33 -0600

    Swords general functions

---
+++ src/main/scala/BasicSword.scala
@@ -0,0 +1,9 @@
+object BasicSword extends Swords {
+
+  val name = "Blacksmith Standard"
+  val basicSwordSum = "The ideal weapon for novice adventurers. Its blade responds swiftly to strike down monsters, but it will break if not handled carefully."
+
+  println(name + basicSwordSum)
+  println(swordRange + swordLevel + basicSwordPrice)
+
+}
