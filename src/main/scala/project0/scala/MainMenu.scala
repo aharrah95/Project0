d4536c698c0ca9c3baff6b1423652a8beb805c0c
aharrah95 <abigail.harrah@revature.net>
2022-01-19 16:17:24 -0600

    Overhaul #1 Main Menu

---
+++ src/main/scala/project0/scala/MainMenu.scala
@@ -0,0 +1,9 @@
+package project0.scala
+
+object MainMenu {
+  def main(args: Array[String]): Unit = {
+    println("Welcome to Bazaar Findings! Ordinary, or bizarre? What will you buy to aid you on your quest?")
+
+  }
+
+}
