859bafcbceb29809d34767ef0e920a049555aec4
aharrah95 <abigail.harrah@revature.net>
2022-01-20 13:30:51 -0600

    Main Menu connect to database attempt

--- src/main/scala/project0/scala/MainMenu.scala
+++ src/main/scala/project0/scala/MainMenu.scala
@@ -1,9 +1,22 @@
 package project0.scala
 import scala.Console.println
+import java.sql.{Connection, DriverManager}
+
 
 object MainMenu {
   def main(args: Array[String]): Unit = {
     Console.println("Welcome to Bazaar Findings! Ordinary, or bizarre? What will you find to aid you on your quest?")
 
+    val con = dbConn()
+
+  }
+  def dbConn(): Connection = {
+    val url = "jdbc:mysql://localhost.3306/bazaar_findings"
+    val username = "root"
+    val password = "M0ch@-Life13"
+    val driver = "com.mysql.jdbc.Driver"
+    val connection = DriverManager.getConnection(url, username, password)
+    println("Successfully connected to..." + connection)
+    connection
   }
 }
