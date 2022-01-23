c05364213ad708d1a9d9076a92252b51555d2ab1
aharrah95 <abigail.harrah@revature.net>
2022-01-23 14:32:54 -0600

    Moved MainMenu.scala

--- src/main/scala/MainMenu.scala
+++ src/main/scala/MainMenu.scala
@@ -1,20 +1,15 @@
-package project0.scala
-import scala.io.StdIn.readLine
-import scala.io.StdIn.readInt
-import java.util.Scanner
-
 object MainMenu {
   def main(args: Array[String]): Unit = {
     Console.println("Welcome to Bazaar Findings! Ordinary, or bizarre? What will you find to aid you on your quest?")
 
     print("What would you like to shop for?")
 
-    val x=scala.io.StdIn.readLine()
+    val x = scala.io.StdIn.readLine()
 
     if (x.equals("Weapons")) {
       println("It's dangerous to go alone! Take this!")
     }
-    else{
+    else {
       if (x.equals("Health Recovery")) {
         println("Our potions will have you feeling better in no time at all!")
       }
